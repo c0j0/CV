2c8994167cb29fd1ef488011771cf45860f7fb32
ALVARO <avaguito1995@hotmail.com>
2024-09-08 21:15:04 +0200

    Actualización del código con correcciones de imágenes y escapado de comillas 8

--- components/Curri3.tsx
+++ components/Curri3.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+/* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
